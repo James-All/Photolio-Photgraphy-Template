4034a9e7d9d2029486afd88a12ac55f6258e67a1
James Allison <james@james.com>
2019-03-11 13:50:58 +0000

    Completed responsiveness, menu overlay and button transitions

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const slideMenu = document.querySelector('.slide-menu');
+const paddingToggle = document.querySelector('#padding-toggle');
+const pictureGallery = document.querySelector('#main-gallery');
+const ourNav = document.querySelector('#menu-items');
+const navItems = document.querySelectorAll('.nav-item');
+
 
 let menuVisible = false;
+let galleryPadding = true;
+let hamburgerSpin = false;
 
 
 //event listeners
@@ -10,10 +17,48 @@ hamburger.addEventListener('click', function(){
     if(!menuVisible){
     hamburger.classList.add('open');
     slideMenu.classList.add('slide-down');
+    ourNav.classList.add('nav-slide');
+    navItems.forEach(item => item.classList.add('nav-item-slide'));
     menuVisible = true;
     }else{
         hamburger.classList.remove('open');
         slideMenu.classList.remove('slide-down');
+        ourNav.classList.remove('nav-slide');
+        navItems.forEach(item => item.classList.remove('nav-item-slide'));
         menuVisible = false;
     }
 });
+
+//toggle for grid padding
+
+paddingToggle.addEventListener('click', function(){
+    console.log('check')
+    if(galleryPadding){
+    pictureGallery.classList.add('remove-padding');
+    galleryPadding = false;
+    }else{
+        pictureGallery.classList.remove('remove-padding');
+        galleryPadding = true;
+    }
+});
+
+//hamburger spin on mouseover/mouseout events
+
+hamburger.addEventListener('mouseover', function(){
+    if(!hamburgerSpin && !menuVisible){
+    hamburger.classList.remove('spin-reverse');
+    hamburger.classList.add('spin');
+    hamburgerSpin = true;
+    }
+})
+hamburger.addEventListener('mouseout', function(){
+    if(hamburgerSpin && !menuVisible){
+    hamburger.classList.remove('spin');
+    hamburger.classList.add('spin-reverse');
+    hamburgerSpin = false;
+    }
+})
+
+
+
+
